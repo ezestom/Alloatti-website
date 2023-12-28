aa4b1eba76931d9c7f270badd4c6753e082311f9
eze stom <ezequielstom@gmail.com>
2023-12-28 17:12:23 -0300

    Update styling in Data component

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -9,7 +9,7 @@ export function Data() {
 					<h2 className=" font-bold md:text-5xl">
 						Manteniendo la confianza de nuestros clientes
 					</h2>
-					<p className="mt-4  sm:text-xl">
+					<p className="mt-4  sm:text-xl text-white/80">
 						Estamos orgullosos de haber brindado un servicio de
 						calidad a nuestros clientes durante más de 30 años.
 						Estos son algunos de nuestros logros.
@@ -17,33 +17,33 @@ export function Data() {
 				</div>
 
 				<div className="mt-8 sm:mt-12 ">
-					<dl className="grid grid-cols-1 gap-4 sm:grid-cols-3 ">
-						<div className="flex flex-col rounded-lg border border-gray-200 px-4 py-8 text-center shadow-md mx-8  ">
-							<dt className="order-last text-lg font-medium text-white">
+					<dl className="grid grid-cols-1 gap-1 sm:grid-cols-3 ">
+						<div className="flex flex-col rounded-lg px-4 py-8 text-center shadow-md border-4 border-white/10 mx-8  ">
+							<dt className="order-last text-lg font-medium text-white/70">
 								Países Alcanzados
 							</dt>
 
-							<dd className="text-4xl font-extrabold text-blue-600 md:text-5xl">
+							<dd className="text-4xl font-extrabold text-white lg:text-6xl">
 								+5
 							</dd>
 						</div>
 
-						<div className="flex flex-col rounded-lg border border-gray-200 px-4 py-8 text-center shadow-md mx-8   ">
-							<dt className="order-last text-lg font-medium text-white">
+						<div className="flex flex-col rounded-lg px-4 py-8 text-center shadow-md border-4 border-white/10 mx-8   ">
+							<dt className="order-last text-lg font-medium text-white/70">
 								Máquinas Vendidas
 							</dt>
 
-							<dd className="text-4xl font-extrabold text-blue-600 md:text-5xl">
+							<dd className="text-4xl font-extrabold text-white lg:text-6xl">
 								+600
 							</dd>
 						</div>
 
-						<div className="flex flex-col rounded-lg border border-gray-200 px-4 py-8 text-center shadow-md mx-8 ">
-							<dt className="order-last text-lg font-medium text-white">
+						<div className="flex flex-col rounded-lg px-4 py-8 text-center shadow-md border-4 border-white/10 mx-8 ">
+							<dt className="order-last text-lg font-medium text-white/70">
 								Clientes Satisfechos
 							</dt>
 
-							<dd className="text-4xl font-extrabold text-blue-600 md:text-5xl">
+							<dd className="text-4xl font-extrabold text-white lg:text-6xl">
 								+400
 							</dd>
 						</div>
